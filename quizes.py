edc1ecdf867a1f961b65f5d2267090d8abcb6a04
Punitkumar Y Harsoor <39958570+punitkmryh@users.noreply.github.com>
2020-01-24 13:27:18 +0530

    Update quizes.py

--- quizes.py
+++ quizes.py
@@ -40,7 +40,6 @@ class BaseQuiz:
 class HARSQuiz(BaseQuiz):
 
     RESULTS = {
-        'ru': ['отсутствие тревоги 👍', 'средняя выраженность тревожного расстройства 😐', 'тяжелая тревога 😦'],
         'en': ['mild anxiety severity', 'mild to moderate anxiety severity', 'moderate to severe anxiety severity']}
 
     type_ = 'hars'
@@ -73,8 +72,7 @@ class HARSQuiz(BaseQuiz):
 class MADRSQuiz(BaseQuiz):
 
     RESULTS = {
-        'en': ['normal 👍', 'mild depression 😐', 'moderate depression 😔', 'severe depression 😨'],
-        'ru': ['норма 👍', 'слабая депрессия 😐', 'умеренная депрессия 😔', 'тяжелая депрессия 😨']}
+        'en': ['normal 👍', 'mild depression 😐', 'moderate depression 😔', 'severe depression 😨']}
 
     type_ = 'madrs'
 
